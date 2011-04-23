f67386a8ec2f538cf12a0b2d0f502a8bd2970b54
Silas <silas@vpcx.(none)>
2011-04-23 22:51:15 +0100

    added creation from triangular line array

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,29 +1,53 @@
 package graph;
+import graph.exception.ArrayNotTriangularException;
+
 import java.util.*;
 
 public class Graph extends HashSet<Vertex> {
+
 	public Graph() {
 		
 	}
 	
-	public static Graph createGraphFromTriangle(){
-		return null;
+	public static Graph createGraphFromTriangle(int[][] triangle) throws ArrayNotTriangularException {
+		if (!isTriangleShaped(triangle)){
+			throw new ArrayNotTriangularException();
+		}
+		
+		return connectVertices(new Vertex(triangle[0][0]), triangle, 0);
+	}
+	
+	private static boolean isTriangleShaped(int[][] triangle){
+		for (int row=0; row < triangle.length; row++){
+			if (triangle[row].length != row + 1){
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	private static Graph connectVertices(Vertex root, int[][] triangle, int index){
-		HashSet<Vertex> children = new HashSet<Vertex>();
+		Graph union = new Graph();
+		if (index > getLastIndexOfRow(triangle.length) - 1) {
+			return union;
+		}
 		int row = getRowFromIndex(index);
 		int column = index - getLastIndexOfRow(row - 1) - 1;
-		children.add(new Vertex(triangle[row + 1][column]));
+		// Add children
+		union.add(new Vertex(triangle[row + 1][column]));
+		union.add(new Vertex(triangle[row + 1][column + 1]));
+		
+		union.addAll(connectVertices(root, triangle, index + 1));
+		return union;
 	}
 	
-	
+	// Use triangle numbers to move between row/column indices and a single left-to-right index
+	// Note the triangle index is naturally 1-based
 	private static int getRowFromIndex(int index){
-		return (int) Math.floor(Math.sqrt(2*index + 0.25) - 0.5) - 1;
+		return (int) Math.ceil(Math.sqrt(2*(index + 1) + 0.25) - 0.5) - 1;
 	}
 	
 	private static int getLastIndexOfRow(int row){
-		return (row + 2)*(row + 1)/2;
-	}
+		return (row + 2)*(row + 1)/2 - 1;
 	}
 }
--- src/trianglepaths/MinTrianglePath.java
+++ src/trianglepaths/MinTrianglePath.java
@@ -1,12 +1,18 @@
 package trianglepaths;
 
+import graph.Graph;
+import graph.exception.ArrayNotTriangularException;
+
 public class MinTrianglePath {
 
 	/**
 	 * @param args
+	 * @throws ArrayNotTriangularException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ArrayNotTriangularException {
+		int[][] triangle = {{1},{2,3},{4,5,6},{7,8,9,10}};
+		Graph graph = Graph.createGraphFromTriangle(triangle);
+		System.out.println("oo");
 
 	}
 
